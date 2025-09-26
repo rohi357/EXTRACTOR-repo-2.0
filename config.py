6f6c382299ab29741ed4a2dbbe4f84c70fac8edc
rohi357 <kumarrohit925721@gmail.com>
2025-09-27 01:44:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,36 +4,36 @@ from os import getenv
 
 
 # ------------------------------------------------
-API_ID = int(os.environ.get("API_ID", "26468828"))
+API_ID = int(os.environ.get("API_ID", "21567814"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","4693513c08d1ac6af15f95b116c29478")
+API_HASH = os.environ.get("API_HASH","cd7dc5431d449fd795683c550d7bfb7e")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # ------------------------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "@wizardd2_bot")
-BOT_TEXT = "Hero Extractor"
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Stranger_txt_extractor_bot")
+BOT_TEXT = "『 THE BOYS / STRANGER BOYS 』"
 # ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "8167879352"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6126688051"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002334419872"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002583554860"))
 
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002583554860")) 
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://chiruedizz:WmzSiQlS35fLDImn@cluster0.4o4zl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002403971925"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002583554860"))
 # -----------------------------------------------
-join = '<a href="https://t.me/+M2aF1-qifHtkMjA1">✳️ JOIN BACKUP</a>'
+join = '<a href="https://t.me/+jjYZLW4sTmIwOTdl">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "wizardd2_bot" #without @
+ADMIN_BOT_USERNAME = "Stranger_txt_extractor_bot" #without @
 
-THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/9HhNmHYg/STRANGER-BOY.jpg")
+THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/ccV44ZRS/STRANGER-BOY.jpg")
 thumb_url = os.environ.get("thumb_url","https://i.ibb.co/q3SX4gjQ/STRANGER-BOY.jpg")
-CONTACT = os.environ.get("CONTACT","[TEAM_JNC](https://t.me/jnc_devoloper)")
-CREDIT = os.environ.get("CREDIT","Team_jnc") #DONT PASTE LINK ONLY BEAST / CHIRU
+CONTACT = os.environ.get("CONTACT","[『STRANGER BOYS 』](https://t.me/Strangerboys24)")
+CREDIT = os.environ.get("CREDIT","『STRANGER』") #DONT PASTE LINK ONLY BEAST / CHIRU
